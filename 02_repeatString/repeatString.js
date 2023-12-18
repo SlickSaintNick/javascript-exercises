5866162c2fc2b9b9dd3b1fe7054935ebfc5eaa60
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2023-12-19 07:13:19 +1030

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n < 0) return "ERROR";
+    let newString = '';
+    for (let i = 0; i < n; i++) {
+        newString += s;
+    }
+    return newString;
 };
 
 // Do not edit below this line
